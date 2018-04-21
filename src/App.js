7042d5fc4d993569c4f24548cab53e82c545c14d
Sabrina Markon <phpsitescripts@outlook.com>
2018-04-21 16:02:13 -0600

    Added ResultCard component toshow house cards from data objects, including title, image, price, and number of guests

--- src/App.js
+++ src/App.js
@@ -30,6 +30,17 @@ class App extends Component {
               queryFormat="basic_date" 
               initialMonth={new Date('04-01-2017')} 
               />
+              <NumberBox 
+                componentId="GuestSensor" 
+                dataField="accomodates" 
+                title="Guests" 
+                defaultSelecte={2} 
+                labelPosition="right" 
+                data={{
+                  start: 1,
+                  end: 16
+                }}
+              />
               <RangeSlider 
                 componentId="PriceSensor" 
                 dataField="price" 
@@ -52,7 +63,33 @@ class App extends Component {
                   and: ['DateRangeSensor'],
                 }} 
               />
-              
+              <ResultCard 
+                className="right-col" 
+                componentId="SearchResult" 
+                dataField="name" 
+                size={12} 
+                onData={data => ({
+                  image: data.image,
+                  title: data.name,
+                  description: (
+                    <div>
+                      <div className="price">${data.price}</div>
+                      <p className="info">{data.room_type} . {data.accommodates} guests</p>
+                    </div>
+                  ),
+                  url: data.listing_url,
+                })} 
+                pagination 
+                react={{
+                  and: ['GuestSensor', 'PriceSensor', 'DateRangeSensor', 'search'],
+                }} 
+                innerClass={{
+                  resultStats: 'result-stats',
+                  list: 'list',
+                  listItem: 'list-item',
+                  image: 'image',
+                }}
+              />
           </div>
         </ReactiveBase>
       </section>

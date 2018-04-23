a5c6c344fc68f0c8b47dda063bd8148b44b15192
Sabrina Markon <phpsitescripts@outlook.com>
2018-04-22 21:52:43 -0600

    fix right column div so images show properly

--- src/App.js
+++ src/App.js
@@ -64,33 +64,34 @@ class App extends Component {
                   and: ['DateRangeSensor'],
                 }} 
               />
-              <ResultCard 
-                className="right-col" 
-                componentId="SearchResult" 
-                dataField="name" 
-                size={12} 
-                onData={data => ({
-                  image: data.image,
-                  title: data.name,
-                  description: (
-                    <div>
-                      <div className="price">${data.price}</div>
-                      <p className="info">{data.room_type} . {data.accommodates} guests</p>
-                    </div>
-                  ),
-                  url: data.listing_url,
-                })} 
-                pagination 
-                react={{
-                  and: ['GuestSensor', 'PriceSensor', 'DateRangeSensor', 'search'],
-                }} 
-                innerClass={{
-                  resultStats: 'result-stats',
-                  list: 'list',
-                  listItem: 'list-item',
-                  image: 'image',
-                }}
-              />
+          </div>
+          <div className="right-col">
+            <ResultCard 
+              componentId="SearchResult" 
+              dataField="name" 
+              size={12} 
+              onData={data => ({
+                image: data.image,
+                title: data.name,
+                description: (
+                  <div>
+                    <div className="price">${data.price}</div>
+                    <p className="info">{data.room_type} . {data.accommodates} guests</p>
+                  </div>
+                ),
+                url: data.listing_url,
+              })} 
+              pagination 
+              react={{
+                and: ['GuestSensor', 'PriceSensor', 'DateRangeSensor', 'search'],
+              }} 
+              innerClass={{
+                resultStats: 'result-stats',
+                list: 'list',
+                listItem: 'list-item',
+                image: 'image',
+              }}
+            />
           </div>
         </ReactiveBase>
       </section>

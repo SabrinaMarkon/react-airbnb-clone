4a5167c943340defe2bb39c1b4bbeec4ec1e43b3
Sabrina Markon <phpsitescripts@outlook.com>
2018-04-22 21:38:32 -0600

    Add styling to page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
           app="react-airbnb-clone-housing" 
           /* 
           Create appbase.io account and
-          clone https://opensource.appbase.io/dejavu/live/#?input_state=XQAAAAJ3AQAAAAAAAAA9iIqnY-B2BnTZGEQz6wkFsW71dAzA7YYc-SS2NBdZOu2iiqUDTwzb8SRY-P60qxz_ZFKoJgMwEJushaRl-FxMxQqDCBLVG-xBlA5HfOZXDzUuGnntd_Zw9u4C0YdVJQ8HvMJrVO8AfQy73d9wq7TjySsVRv-NAKU5ZUw4jxU0ynrQflgPkDLN6AGDv4jeOi8Ir9BBSZ-bdv4J2oq7eCzLoC-gl9qTZsTRLHsXPhHvClG5we6nqctwdPgHqEWqj25nG0qo1RkmJYY_ZTF4XEJcMQyIw-2Rck0OE-ZTR7g8d3ste2uR2u9JbeJj9fjtiWHVAv4cBigXZG0RRBK2__eggwE&editable=false to get your own credentials for the bwlow credentials properties.
+          clone https://opensource.appbase.io/dejavu/live/#?input_state=XQAAAAJ3AQAAAAAAAAA9iIqnY-B2BnTZGEQz6wkFsW71dAzA7YYc-SS2NBdZOu2iiqUDTwzb8SRY-P60qxz_ZFKoJgMwEJushaRl-FxMxQqDCBLVG-xBlA5HfOZXDzUuGnntd_Zw9u4C0YdVJQ8HvMJrVO8AfQy73d9wq7TjySsVRv-NAKU5ZUw4jxU0ynrQflgPkDLN6AGDv4jeOi8Ir9BBSZ-bdv4J2oq7eCzLoC-gl9qTZsTRLHsXPhHvClG5we6nqctwdPgHqEWqj25nG0qo1RkmJYY_ZTF4XEJcMQyIw-2Rck0OE-ZTR7g8d3ste2uR2u9JbeJj9fjtiWHVAv4cBigXZG0RRBK2__eggwE&editable=false to get your own credentials for the 'credentials' property below.
+          Note that the sample data set is from Airbnb Seattle listings. If you want to use this for a live site you need to build up your own data of course.
           */
           credentials="s0UZYa1lr:1ed8e0e1-a640-4317-b3db-021dffbc82e8" 
           type="listing" 
@@ -19,7 +20,7 @@ class App extends Component {
           }}
         >
           <nav className="nav">
-            <div className="title">Airfun</div>
+            <div className="title">AirFuN</div>
           </nav>
           <div className="left-col">
             <DateRange
